e20c59c45703fd4c5695d3115ccc0d047dcb0c4e
Eliu Florez <eliufz@gmail.com>
2019-02-28 14:54:17 -0800

    TreeBuilder > 4.2
    Symfony TreeBuilder > 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -29,10 +29,15 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('fos_oauth_server');
 
-        /** @var ArrayNodeDefinition $rootNode */
-        $rootNode = $treeBuilder->root('fos_oauth_server');
+        // symfony > 4.2 support
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else { 
+            // symfony < 4.2 support
+            $rootNode = $treeBuilder->root('fos_oauth_server');
+        }
 
         $supportedDrivers = ['orm', 'mongodb', 'propel', 'custom'];
 
